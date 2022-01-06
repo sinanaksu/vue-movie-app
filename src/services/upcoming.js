56975f6fed678c7a7f0e4979e0e03db72f7e484a
Sinan Aksu <sinanksu@gmail.com>
2022-01-07 02:10:00 +0300

    feat: add upcoming service

---
+++ src/services/upcoming.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { apiURL, apiHeaders } from "../utils/";
+
+export const upcoming = (mediaType, locale) => {
+  console.log(apiURL)
+  return axios.get(
+    apiURL + "/movie/upcoming/" + mediaType + "?language=" + locale,
+    { headers: apiHeaders }
+  );
+};
