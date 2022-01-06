864b998de14bb83f04f3194bcc4026f18c3e558c
Sinan Aksu <sinanksu@gmail.com>
2022-01-07 02:08:56 +0300

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <transition name="fade" mode="out-in">
       <router-view />
     </transition>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Navbar,
+    Footer
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      Powered By 2022 @ <a href="https://github.com/sinanaksu/vue-movie-app" target="_blank">Github Repo</a>
+    </div>
+  </div>
+</template>
