0bdc6a24d67141a9b2bdd108eb87a3f17e8b61e2
Sinan Aksu <sinanksu@gmail.com>
2022-01-07 01:45:03 +0300

    style: add background img jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-5 mb-4 mt-n3 bg-light rounded-3">
+    <div class="home-jb p-5 mb-4 mt-n3 rounded-3">
       <div class="container py-2">
         <h1 class="fw-bold">{{ $t("jumboton_title") }}</h1>
         <p class="col-md-8 fs-4">{{ $t("jumboton_desc") }}</p>
