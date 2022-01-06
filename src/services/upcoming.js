af8b5f165e5d513a1f4212e91d0ea679422eb452
Sinan Aksu <sinanksu@gmail.com>
2022-01-07 02:56:39 +0300

    fix: update upcoming api path

--- src/services/upcoming.js
+++ src/services/upcoming.js
@@ -4,7 +4,7 @@ import { apiURL, apiHeaders } from "../utils/";
 export const upcoming = (mediaType, locale) => {
   console.log(apiURL)
   return axios.get(
-    apiURL + "/movie/upcoming/" + mediaType + "?language=" + locale,
+    apiURL + "/movie/upcoming" + mediaType + "?language=" + locale,
     { headers: apiHeaders }
   );
 };
